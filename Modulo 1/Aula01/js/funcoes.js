0b16914bdb9883d6bf0584f1c5665ffdf2cafbab
SilasDias <silasdjd@gmail.com>
2020-07-14 13:10:57 -0300

    Add Aula07

---
+++ Modulo 1/Aula01/js/funcoes.js
@@ -0,0 +1,22 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log(sum(1, 2));
+
+// function compareNumbers(a, b) {
+//   // return a > b ? 1 : a < b ? -1 : 0;
+//   return a - b;
+// }
+// console.log(compareNumbers(1, 1));
+// console.log(compareNumbers(1, 2));
+// console.log(compareNumbers(2, 1));
+
+// function somatorio(from, upTo) {
+//   var sum = 0;
+
+//   for (var i = from; i <= upTo; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+// console.log(somatorio(1, 10));
---
+++ Modulo 1/Aula01/js/script.js
@@ -0,0 +1,8 @@
+// Operador Tenário
+var a = 8;
+var b = 7;
+var resposta = a > b ? 'maior' : a < b ? 'menor' : 'igual';
+console.log(resposta);
+
+var element = document.querySelector('p');
+element.textContent = 'mudei o texto';
