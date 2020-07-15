842c81374bb40287b8268c6a5ca4588e92c2d4b3
M0nk3ydr4k3 <m0nk3y@localhost.localdomain>
2020-07-14 21:01:37 -0300

    Aula 07

--- Modulo 1/aula07/aula/js/script.js
+++ Modulo 1/aula07/aula/js/script.js
@@ -1,1 +1,34 @@
-console.log('Oi');
+// var element = document.querySelector('h1');
+// element.textContent = 'Mudar o texto aqui';
+
+// var personalDataArray = document.querySelectorAll('.personal-Data');
+// personalDataArray;
+// personalDataArray = Array.from(personalDataArray);
+
+// var data = Array.from(document.querySelectorAll('.data'));
+// for (var i = 0; i < data.length; i++) {
+//   var currentElement = data[i];
+//   currentElement.classList.add('emphasis');
+//   // currentElement.style.color = 'green';
+// }
+// var p2 = document.querySelector('#p1');
+// var p2 = document.querySelector('#p2');
+// var p3 = document.querySelector('#p3');
+
+// p1.classList.add('good');
+// p2.classList.add('great');
+// p3.classList.add('awesome');
+
+if (id > 0 && id <= filmes.length) {
+  var add = id - 1;
+  return (
+    personagens[add] +
+    ' é um personagem do filme ' +
+    filmes[add] +
+    ' que estreou no cinema em ' +
+    lancamentos[add] +
+    '.'
+  );
+} else {
+  return 'Essa não é uma opção válida.';
+}
