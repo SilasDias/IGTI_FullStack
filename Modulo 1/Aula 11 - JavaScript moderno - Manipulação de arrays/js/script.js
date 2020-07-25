85d8c26eb74d544f4c20dfc5283466ec8a7d3fe5
SilasDias <silasdjd@gmail.com>
2020-07-25 19:41:29 -0300

    Tipos de Arrays

--- Modulo 1/Aula 11 - JavaScript moderno - Manipulação de arrays/js/script.js
+++ Modulo 1/Aula 11 - JavaScript moderno - Manipulação de arrays/js/script.js
@@ -6,3 +6,34 @@
 // some - verifica se há pelo menos um elemento que atenda à proposição.
 // every - verifica se todos os elementos atendem à proposição.
 // sort - ordena os elementos com base em um critério.
+
+// (✓) map      (transformar array em objeto com nome e email)
+
+window.addEventListener('load', () => {
+  doMap();
+  doFilter();
+  doForEach();
+});
+
+function doMap() {
+  const nameEmailArray = people.results.map((person) => {
+    return {
+      name: person.name,
+      email: person.email,
+    };
+  });
+  console.log(nameEmailArray);
+}
+
+// (✓) filter   (maiores de 18 anos)
+
+function doFilter() {
+  oldThan50 = people.results.filter((person) => {
+    return person.dob.age > 50;
+  });
+  console.log(oldThan50);
+}
+
+// (✓) forEach  (incluir nova propriedade no objeto)
+
+function doForEach() {}
