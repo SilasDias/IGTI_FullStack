5f3769eaba544bcb3606281715996cbd87607531
SilasDias <silasdjd@gmail.com>
2020-08-17 13:04:45 -0300

    Cors

--- Modulo 2/Aula 3 Express: Middlewares, tratamento de erros e gravação de logs/MyBankAPI/index.js
+++ Modulo 2/Aula 3 Express: Middlewares, tratamento de erros e gravação de logs/MyBankAPI/index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import winston from 'winston';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerDocument } from './doc.js';
 import accountsRouter from './routes/accounts.js';
 import { promises as fs } from 'fs';
 
@@ -6,16 +10,31 @@ const { readFile, writeFile } = fs;
 
 global.fileName = 'accounts.json'; // variavel global
 
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message} `;
+});
+global.logger = winston.createLogger({
+  level: 'silly',
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'mybankapi.log' }),
+  ],
+  formart: combine(label({ label: 'mybankapi' }), timestamp(), myFormat),
+});
+
 const app = express();
 app.use(express.json());
-
+app.use(cors());
+app.use(express.static('public'));
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/accounts', accountsRouter);
 
 // Arquivo de controle de fluxo
 app.listen(3000, async () => {
   try {
     await readFile(global.fileName);
-    console.log('API Started!');
+    logger.info('API Started!');
   } catch (err) {
     const iniatialJason = {
       nextId: 1,
@@ -23,10 +42,10 @@ app.listen(3000, async () => {
     };
     writeFile(global.fileName, JSON.stringify(iniatialJason))
       .then(() => {
-        console.log('API Started and File Created!');
+        logger.info('API Started and File Created!');
       })
       .catch((err) => {
-        console.log(err);
+        logger.error(err);
       });
   }
 });
